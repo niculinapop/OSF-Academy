5fa3bf8713e65f65e81d67d612d6d64bdc2e072e
Niculina Pop <niculinapop14@gmail.com>
2025-02-21 14:06:55 +0200

    sending data exercise
    sending and displaying data in HelloWorld route

--- cartridges/app_training/cartridge/controllers/Training.js
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -1,12 +1,12 @@
-'use strict'
+'use strict';
 
 var server =require('server');
 
 server.get('HelloWorld', function (req, res, next) {
-    var viewData = res.getViewData();
-    viewData.myvariable = 32;
-    res.setViewData(viewData);
-    res.render("training/myfirsttemplate", { viewData: viewData });
+    var myVariable = "Just a string";
+
+    res.render("training/myfirsttemplate", {myVariable: myVariable });
+
     return next();
 });
 module.exports = server.exports();
