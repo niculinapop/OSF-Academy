4d6fafec8bd2acbdbef2842b900174ef17fc2952
Niculina Pop <niculinapop14@gmail.com>
2025-02-21 11:57:42 +0200

    add Training.js and myfirsttemplate.isml

---
+++ cartridges/app_training/cartridge/controllers/Training.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var server =require('server');
+
+server.get('HelloWorld', function (req, res, next) {
+    var viewData = res.getViewData();
+    viewData.myvariable = 32;
+    res.setViewData(viewData);
+    res.render("training/myfirsttemplate", { viewData: viewData });
+    return next();
+});
+module.exports = server.exports();
