3c3004da07bf8888edfcb8bc5b2f313f232495ad
Niculina Pop <niculinapop14@gmail.com>
2025-03-08 15:51:46 +0200

    added content asset and updated the Cart.js

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -6,7 +6,7 @@ server.extend(module.superModule);
 server.append('Show', function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
     var currentBasket = BasketMgr.getCurrentBasket();
-    var cartTotal = 0;
+    var cartTotal = 0; 
     var message = '';
 
     if (currentBasket) {
@@ -14,14 +14,23 @@ server.append('Show', function (req, res, next) {
 
         // Check if the cart total exceeds $200
         if (cartTotal > 200) {
-            message = 'Your cart total exceeds $200!'; 
+            var ContentMgr = require('dw/content/ContentMgr');
+            var cartTotalThreshold = ContentMgr.getContent('cartTotalThreshold');
+            
+            if(cartTotalThreshold) {
+                message = cartTotalThreshold.custom.body; 
+            } else {
+                message = 'Your cart total exceeds $200!';
+        
+            }
         }
+        
     }
 
     res.setViewData({
         cartTotal: cartTotal,
         customMessage: message
-    });
+    }); 
  
     next();
 });
